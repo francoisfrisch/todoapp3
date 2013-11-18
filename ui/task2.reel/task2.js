8adecd8855416479ad6f6263974518df5b60b66d
François Frisch <noreply>
2013-11-18 14:56:07 -0800

    Add component task2

---
+++ ui/task2.reel/task2.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/task2.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Task2
+ * @extends Component
+ */
+exports.Task2 = Component.specialize(/** @lends Task2# */ {
+    constructor: {
+        value: function Task2() {
+            this.super();
+        }
+    }
+});
